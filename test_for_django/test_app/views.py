a6766fab7bf1e3b021766b8d9c906169a2fa4120
razzl <razzlfor@gmail.com>
2016-02-17 13:51:50 +0800

    add the function of add,only a test

--- test_for_django/test_app/views.py
+++ test_for_django/test_app/views.py
@@ -1,7 +1,15 @@
-#coding:utf-8
+# #coding:utf-8
+# from django.shortcuts import render
+# from django.http import HttpResponse
+# # Create your views here.
+
+# def index(request):
+# 	return HttpResponse(u"这只是一个简单的测试")
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
 
-def index(request):
-	return HttpResponse(u"这只是一个简单的测试")
+def add(request):
+    a = request.GET['a']
+    b = request.GET['b']
+    c = int(a) + int(b)
+    return HttpResponse(str(c))
--- test_for_django/test_for_django/urls.py
+++ test_for_django/test_for_django/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'test_for_django.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$','test_app.views.index',name='home'),
+    url(r'^add/$','test_app.views.add',name='add'),
+    #url(r'^$','test_app.views.index',name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
