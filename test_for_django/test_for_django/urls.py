a56faabe320c4fd0762aa285b655786706d74824
razzl <razzlfor@gmail.com>
2016-02-24 09:22:44 +0800

    templates 5

--- test_for_django/test_for_django/urls.py
+++ test_for_django/test_for_django/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     # url(r'^add/$','test_app.views.add',name='add'),
     #url(r'^$','test_app.views.index',name='home'),
-    url(r'^add/(\d+)/(\d+)/$','test_app.views.add2',name='add'),
+    url(r'^add/(\d+)/(\d+)/$','test_app.views.add',name='add'),
     # url(r'^$','test_app.views.home',name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
