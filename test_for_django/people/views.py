24ee6dda06b11b36b2f6d16c4a3cca8d48fd0098
razzl <razzlfor@gmail.com>
2016-03-12 10:32:27 +0800

    test1 database

--- test_for_django/people/views.py
+++ test_for_django/people/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from people.models import Person
+
+def testdb(request):
+    test1 = Person(name='razzl',age='8')
+    test1.save()
+    return HttpResponse("the data is save now!")
