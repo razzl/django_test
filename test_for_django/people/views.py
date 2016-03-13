ba038675d8791ec9f5067acb1caa7493a556f4a0
razzl <razzlfor@gmail.com>
2016-03-13 09:50:47 +0800

    test database

--- test_for_django/people/views.py
+++ test_for_django/people/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,6 +6,27 @@ from django.http import HttpResponse
 from people.models import Person
 
 def testdb(request):
-    test1 = Person(name='razzl',age='8')
-    test1.save()
-    return HttpResponse("the data is save now!")
+    # test1 = Person(name='razzl',age='8')
+    # test1.save()
+    # return HttpResponse("the data is save now!")
+    response = ""
+    response1 = ""
+    data = Person.objects.all()
+    #利用objects这个模型管理器的all获得所有的数据 相当于select * from ***
+    response2 = Person.objects.filter(id=1)
+    #filter 相当于sql中的where过滤条件
+    response3 = Person.objects.get(id=1)
+    #获取单个对象
+    Person.objects.order_by('name')[0:2]
+    # 限制返回的数据 相当于 SQL 中的 OFFSET 0 LIMIT 2;
+    #数据排序
+    Test.objects.order_by("id")
+    
+    # 上面的方法可以连锁使用
+    Test.objects.filter(name="razzl").order_by("id")
+    
+    # 输出所有数据
+    for var in list:
+        response1 += var.name + " "
+    response = response1
+    return HttpResponse("<p>" + response + "</p>")
