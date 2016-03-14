10444c2fac0ef72930659b64778181f589fb428c
razzl <razzlfor@gmail.com>
2016-03-14 14:42:03 +0800

    test blog

--- test_for_django/test_for_django/urls.py
+++ test_for_django/test_for_django/urls.py
@@ -7,5 +7,5 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$','blog.views.home',name='home'),
-    url(r'^blog/',include('blog.urls')),
+    # url(r'^blog/',include('blog.urls')),
 )
