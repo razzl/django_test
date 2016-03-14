98c6141647efe76a57eb299f54547cde1bcec10e
razzl <razzlfor@gmail.com>
2016-03-14 14:33:27 +0800

    test blog

--- test_for_django/test_for_django/urls.py
+++ test_for_django/test_for_django/urls.py
@@ -5,15 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'test_for_django.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    # url(r'^add/$','test_app.views.add',name='add'),
-    #url(r'^$','test_app.views.index',name='home'),
-    # url(r'^add/(\d+)/(\d+)/$','test_app.views.add',name='add'),
-    # url(r'^$','test_app.views.home',name='home'),
     url(r'^admin/', include(admin.site.urls)),
-    # url(r'^testdb/$','people.views.testdb',name='testdb'),
-    # url(r'^search_form/$','people.views.search_form',name='search_form'),
-    # url(r'^search/$','people.views.search',name='search'),
-    # url(r'^search-post/$','people.views.search_post',name='search-post'),
+    url(r'^$','blog.views.home',name='home'),
+    url(r'^blog/',include('blog.urls')),
 )
