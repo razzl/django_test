61c3059ccfb3efb05291bbbd50e8be5bbe0c18e7
razzl <razzlfor@gmail.com>
2016-03-18 14:48:35 +0800

    sql injection app

--- test_for_django/sql_injection/admin.py
+++ test_for_django/sql_injection/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from sql_injection import Sql_test_user
+from sql_injection.models import Sql_test_user
 # Register your models here.
 admin.site.register(Sql_test_user)
