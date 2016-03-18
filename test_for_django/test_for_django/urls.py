d333d1aa2daebeb5c1c6a301b1e9209aca886c7c
razzl <razzlfor@gmail.com>
2016-03-18 14:46:04 +0800

    sql injection app

--- test_for_django/test_for_django/urls.py
+++ test_for_django/test_for_django/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls')),
+    url(r'^sql/', include('sql_injection.urls')),
     url(r'^$','blog.views.home',name='home'),
     # url(r'^blog/',include('blog.urls')),
 )
