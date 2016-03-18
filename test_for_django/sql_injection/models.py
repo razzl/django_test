d0dd81a39e53c281961aabc6ed375e9a65b4db50
razzl <razzlfor@gmail.com>
2016-03-18 14:42:49 +0800

    sql injection app

--- test_for_django/sql_injection/models.py
+++ test_for_django/sql_injection/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Sql_test_user(models.Models):
+class Sql_test_user(models.Model):
     user_name = models.CharField(max_length=200)
     user_passwd = models.CharField(max_length=200)
     user_age = models.IntegerField(default=0)
