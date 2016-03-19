967137c4e10b05cf2ca9fd8ccf15baef5ed78487
razzl <razzlfor@gmail.com>
2016-03-19 09:44:59 +0800

    raw sql

--- test_for_django/sql_injection/views.py
+++ test_for_django/sql_injection/views.py
@@ -8,6 +8,6 @@ def get_user(request):
     name = request.GET['name']
     # user = Sql_test_user.objects.get(user_name = name)
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM  sql_injection_sql_test_user WHERE user_name = " + name)
+    cursor.execute("SELECT * FROM  sql_injection_sql_test_user WHERE user_name ="+"'" + name+"'")
     user = cursor.fetchall()
     return render(request,'sql_test_user.html',{'user':user})
