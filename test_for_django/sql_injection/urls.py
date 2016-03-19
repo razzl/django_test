94cff58c2b55e12d06bfdb1992cf04e18d24715b
razzl <razzlfor@gmail.com>
2016-03-19 18:27:21 +0800

    raw sql

--- test_for_django/sql_injection/urls.py
+++ test_for_django/sql_injection/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # url(r'^(?P<name>\w+)/$', 'sql_injection.views.get_user', name='get_user'),
-    url(r'^$', 'sql_injection.views.get_user', name='get_user'),
+    url(r'^1/$', 'sql_injection.views.get_user', name='get_user'),
 )
