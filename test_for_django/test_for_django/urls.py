4bc5df7833159039a2cc02b96b2a0761fad4e78b
razzl <razzlfor@gmail.com>
2016-03-19 18:32:43 +0800

    raw sql

--- test_for_django/test_for_django/urls.py
+++ test_for_django/test_for_django/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^blog/', include('blog.urls')),
+    url(r'^test/', include('blog.urls')),
     url(r'^sql/', include('sql_injection.urls')),
     url(r'^$','blog.views.home',name='home'),
     # url(r'^blog/',include('blog.urls')),
