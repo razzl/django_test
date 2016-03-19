06918b5aa72a1a7d063473925ae1e65a07e074f7
razzl <razzlfor@gmail.com>
2016-03-19 09:08:23 +0800

    raw sql

--- test_for_django/sql_injection/views.py
+++ test_for_django/sql_injection/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from sql_injection.models import Sql_test_user
 from django.http import HttpResponse
+from django.db import connection
 # Create your views here.
 
 def get_user(request):
     name = request.GET['name']
-    user = Sql_test_user.objects.get(user_name = name)
-    return HttpResponse(user.user_name+user.user_passwd+"123")
+    # user = Sql_test_user.objects.get(user_name = name)
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM  sql_injection_sql_test_user WHERE user_name = name")
+    user = [row[0] for row in cursor.fetchall()]
+    return HttpResponse('username:'user[user_name]+'\n'+'user password:'+user[user_passwd])
