6e54735bd20e5a59f5bed6e0eb4ce1e3adf8ceff
razzl <razzlfor@gmail.com>
2016-03-20 14:22:10 +0800

    raw sql

--- test_for_django/sql_injection/models.py
+++ test_for_django/sql_injection/models.py
@@ -5,3 +5,5 @@ class Sql_test_user(models.Model):
     user_name = models.CharField(max_length=200)
     user_passwd = models.CharField(max_length=200)
     user_age = models.IntegerField(default=0)
+    def __unicode__(self):
+        return self.user_name
