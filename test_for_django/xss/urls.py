c38a9e51382abb38a954d40a4d80fbb02442f0de
razzl <razzlfor@gmail.com>
2016-03-20 18:27:41 +0800

    raw sql

--- test_for_django/xss/urls.py
+++ test_for_django/xss/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
     # url(r'^(?P<name>\w+)/$', 'sql_injection.views.get_user', name='get_user'),
     url(r'^1/$', 'xss.views.xss_1', name='xss_1'),
+    url(r'^2/$', 'xss.views.xss_2', name='xss_2'),
 )    
--- test_for_django/xss/views.py
+++ test_for_django/xss/views.py
@@ -4,13 +4,15 @@ from django.http import HttpResponse
 from django.db import connection
 # Create your views here.
 from django.template import Context, Template
+import re
 def xss_1(request):
+    t = Template('<h2>if you input something, it will show on the page:{{ name|safe }}</h2>')
+    c = Context({'name': name})
+    return HttpResponse(t.render(c))
+
+def xss_2(request):
     name = request.GET['name']
-    # user = Sql_test_user.objects.get(user_name = name)
-    # cursor = connection.cursor()
-    # cursor.execute("SELECT * FROM  sql_injection_sql_test_user WHERE user_name ="+"'" + name+"'")
-    # user = cursor.fetchall()
-    # return HttpResponse("<h2>{{ name|safe }}</h2>")
+    name = re.sub(r'script','',name)
     t = Template('<h2>if you input something, it will show on the page:{{ name|safe }}</h2>')
     c = Context({'name': name})
     return HttpResponse(t.render(c))
