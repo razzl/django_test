ae0524e7fc97f96645a11bc3e02b81c91a474241
razzl <razzlfor@gmail.com>
2016-03-21 15:41:54 +0800

    integer sql test

--- test_for_django/sql_injection/urls.py
+++ test_for_django/sql_injection/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('',
     # url(r'^(?P<name>\w+)/$', 'sql_injection.views.get_user', name='get_user'),
     url(r'^1/$', 'sql_injection.views.get_user', name='get_user'),
     url(r'^2/$', 'sql_injection.views.get_user_2', name='get_user_2'),
+    url(r'^3/$', 'sql_injection.views.get_user_3', name='get_user_3'),
 )    
--- test_for_django/sql_injection/views.py
+++ test_for_django/sql_injection/views.py
@@ -3,7 +3,7 @@ from sql_injection.models import Sql_test_user
 from django.http import HttpResponse
 from django.db import connection
 # Create your views here.
-
+import re
 def get_user(request):
     name = request.GET['name']
     # user = Sql_test_user.objects.get(user_name = name)
@@ -20,5 +20,14 @@ def get_user_2(request):
             return HttpResponse("<h2>There is some spaces in your string!</h2>")
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM  sql_injection_sql_test_user WHERE user_name ="+"'" + name+"'")
+    user = cursor.fetchall()
+    return render(request,'sql_test_user.html',{'user':user})
+def get_user_3(request):
+    userid = request.GET['id']
+    pattern = re.compile(r'^\d+$',re.M)
+    if !pattern.match(userid):
+        return HttpResponse("<h2>Your input id is not a integer!</h2>")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM  sql_injection_sql_test_user WHERE id =" + userid)
     user = cursor.fetchall()
     return render(request,'sql_test_user.html',{'user':user})
